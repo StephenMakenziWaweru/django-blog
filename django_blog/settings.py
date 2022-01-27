145049c9d7f87b93bcf43cf79a266d781395316a
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-01-27 17:31:39 +0300

    added  django-on-heroku

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,4 +132,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
